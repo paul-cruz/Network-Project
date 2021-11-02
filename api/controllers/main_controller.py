23d97540fdcee3170632198d1a2356c6fee11bf0
Miguel Islas Hernández <43648421+MiguelIslasH@users.noreply.github.com>
2021-11-02 14:51:55 -0600

    Added telnet support

--- api/controllers/main_controller.py
+++ api/controllers/main_controller.py
@@ -5,8 +5,17 @@ class DeviceController:
     driver = napalm.get_network_driver('ios')
     optional_args = {}
     optional_args['dest_file_system'] = 'nvram:'
-    device = driver(hostname=ip, username=user, password=password, optional_args=optional_args)
-
+    try:
+        device = driver(hostname=ip, username=user, password=password, optional_args=optional_args)
+    except:
+        print("No SSH Supported trying with telnet...")
+        
+    optional_args['transport'] = 'telnet'
+    try:
+        device = driver(hostname=ip, username=user, password=password, optional_args=optional_args)
+    except:
+        print("No SSH and no telnet support...")
+       
     print('Opening...')
     device.open()
     return device
