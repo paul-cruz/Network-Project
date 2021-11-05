266e73eefb18852e472365c856a7e9aadc30632c
MiguelIslasH <maih_40@outlook.com>
2021-11-04 18:26:44 -0600

    Feature: Get alll Ips

--- api/controllers/main_controller.py
+++ api/controllers/main_controller.py
@@ -1,4 +1,5 @@
 import napalm
+import json
 
 class DeviceController:
   def prepareDevice(self, ip: str, user: str, password: str):
@@ -9,13 +10,41 @@ class DeviceController:
         device = driver(hostname=ip, username=user, password=password, optional_args=optional_args)
     except:
         print("No SSH Supported trying with telnet...")
-        
+
     optional_args['transport'] = 'telnet'
     try:
         device = driver(hostname=ip, username=user, password=password, optional_args=optional_args)
     except:
         print("No SSH and no telnet support...")
-       
-    print('Opening...')
     device.open()
+    print("Opening %s..."%ip)
     return device
+
+  def getAllDevices(self, user: str, password: str, ip: str):
+    devices = []
+    device = self.prepareDevice(ip, user, password)
+    arpTable = device.get_arp_table()
+    for register in arpTable:
+      if register["age"] != -1.0:
+        if register["ip"] not in devices:
+          devices.append(register["ip"])
+    device.close()
+
+    for deviceIp in devices:
+      try:
+        newDevice = self.prepareDevice(deviceIp, user, password)
+        newArpTable = newDevice.get_arp_table()
+        for register in newArpTable:
+          if register["age"] != -1.0:
+            if register["ip"] not in devices:
+              devices.append(register["ip"])
+        newDevice.close()
+      except Exception as e:
+        print(e)
+        continue
+    print(devices)
+    return devices
+
+#Usage
+#mainController =  DeviceController()
+#mainController.getAllDevices("cisco", "cisco", "192.168.10.254")
