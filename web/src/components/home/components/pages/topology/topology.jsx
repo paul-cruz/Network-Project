989b6fcfc127ffde85660ae5075cc481b00a51af
MiguelIslasH <maih_40@outlook.com>
2021-11-07 21:59:10 -0600

    Activate and desactivate RSA

--- web/src/components/home/components/pages/topology/topology.jsx
+++ web/src/components/home/components/pages/topology/topology.jsx
@@ -1,22 +1,42 @@
+import { useState, useRef } from "react";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function Topology() {
+  const [isRunningCommand, setIsRunningCommand] = useState(false);
+  const protocolRef = useRef();
+
+  const onChangeProtocol = () => {
+    setIsRunningCommand(true);
+    console.log(protocolRef.current.value);
+  };
+
   return (
     <Container className="mt-3 mb-3">
       <Row>
-        <Col className="col-md-8">
+        <Col className="col-md-6">
           <h3>Topología detectada</h3>
         </Col>
         <Col className="col-md-2">Tipo de enrutamiento: </Col>
         <Col className="col-md-2">
-          <Form.Select aria-label="Default select example">
-            <option value="1">RIP</option>
-            <option value="2">OSPF</option>
-            <option value="3">EIGRP</option>
+          <Form.Select aria-label="Default select example" ref={protocolRef}>
+            <option value="RIP">RIP</option>
+            <option value="OSPF">OSPF</option>
+            <option value="EIGRP">EIGRP</option>
           </Form.Select>
+        </Col>
+        <Col className="col-md-2">
+          <Button
+            disabled={isRunningCommand}
+            variant="success"
+            onClick={onChangeProtocol}
+          >
+            Activar
+          </Button>
         </Col>
       </Row>
     </Container>
